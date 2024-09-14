7d38f0bf6820ec9588aaa55e7f9133829c0a3627
RenatinhoCRT <128293838+RenatinhoCRT@users.noreply.github.com>
2024-09-13 22:17:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function markAsBought(button) {
+    const listItem = button.parentElement;
+    listItem.classList.add('bought');
+    button.textContent = "Comprado!";
+    button.disabled = true;
+}
